9269c982b000717320c079a5814c06dccfa60ef1
Anupam mathur <45711832+anupam9546@users.noreply.github.com>
2022-09-03 14:18:26 +0530

    Create basic_calculator.java

---
+++ basic_calculator.java
@@ -0,0 +1,41 @@
+import java.io.*;
+  
+class basic_calculator {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 10, num2 = 20, sum = 0;
+  
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // adding num1 and num2
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+        
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // subtracting num1 and num2
+        sub = num1 - num2;
+        System.out.println("Subtraction = " + sub);
+        
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // Multiplying num1 and num2
+        mult = num1 * num2;
+        System.out.println("Multiplication = " + mult);
+        
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // Dividing num1 and num2
+        div = num1 / num2;
+        System.out.println("Division = " + div);
+        
+    }
+}
